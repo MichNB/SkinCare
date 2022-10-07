d3796a6e91eadb565cb2c1bf1b703fea4f94c5e3
m.navarro <m.navarro@MNAVARRO-L.aligntech.com>
2022-10-07 01:06:57 -0600

    Tarea semana 3, skincare curso viernes IIIC 2022

---
+++ src/test/java/com/skincare/SkinCareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skincare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinCareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
